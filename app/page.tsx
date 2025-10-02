a00a20aea9cd990ff6889d1d30e7d4f7d44d22db
math3wsl3vi <levimathews9841@gmail.com>
2025-10-02 03:04:54 +0300

    page fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import FeaturedDestinations from '@/components/home/FeaturedDestinations';
-import TourPackages from '@/components/home/TourPackages';
-import Testimonials from '@/components/home/Testimonials';
 import CallToAction from '@/components/home/CallToAction';
+import FeaturedDestinations from '@/components/home/FeaturedDestinations';
 import Hero from '@/components/home/Hero';
+import Testimonials from '@/components/home/Testimonials';
+import TourPackages from '@/components/home/TourPackages';
+import React from 'react';
 const Home = () => {
   return <div className="w-full font-poppins">
       <Hero />
